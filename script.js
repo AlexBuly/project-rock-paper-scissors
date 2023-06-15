7e18d4bbf90f9ffe5ddc980f6d8a5568bd0b9cb4
Alex Buly <asbuly@icloud.com>
2023-06-15 15:27:40 -0400

    Back to class

--- script.js
+++ script.js
@@ -30,7 +30,7 @@
          const rockBtn = document.getElementById("rock");
          const paperBtn = document.getElementById("paper");
          const scissorsBtn = document.getElementById("scissors");
-         const results = document.querySelectorAll("#round-results");
+         const results = document.querySelectorAll(".round-results");
 
          rockBtn.addEventListener("click", () => {
             playerSelection = "Rock";
